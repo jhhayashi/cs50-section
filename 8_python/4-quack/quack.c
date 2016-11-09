e39cb353585f3d7ef769bc1bc4b5ba770465dcf5
Jordan Hayashi <jhhayashi@gmail.com>
2016-11-09 14:36:49 -0500

    Add python code

---
+++ 8_python/4-quack/quack.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    while(true)
+    {
+        printf("Quack!\n");
+        sleep(1);
+    }
+}
---
+++ 8_python/4-quack/quack.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("Quack!")
+    time.sleep(1)
